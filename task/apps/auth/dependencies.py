3406d1ed8547f529aff6e8e973ea0cc6e97ad9d7
Daniil Golubev <dangol0610@gmail.com>
2025-11-19 11:27:31 +0300

    Авторизация в Swagger теперь по токену

--- task/apps/auth/dependencies.py
+++ task/apps/auth/dependencies.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import JWTError
 from task.apps.auth.connector import Connector
 from task.apps.auth.repository import AuthRepository
@@ -16,20 +16,29 @@ def get_auth_service(repository: AuthRepository = Depends(get_repository)):
     return AuthService(repository)
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+security = HTTPBearer(auto_error=False)
 
 
 async def get_current_user(
     service: AuthService = Depends(get_auth_service),
-    token: str = Depends(oauth2_scheme),
+    credentials: HTTPAuthorizationCredentials = Depends(security),
 ) -> UserAddDTO:
+    if not credentials:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated"
+        )
+    token = credentials.credentials
     try:
         payload = service.decode_token(token)
         if not payload:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token payload"
             )
-        username = payload["sub"]
+        username = payload.get("sub")
+        if not username:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token payload"
+            )
         user = await service.repository.get_by_username(username)
         if not user:
             raise HTTPException(
